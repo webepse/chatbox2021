af5f55b1debe23992528da28e3b78ded87f9fea7
Boderio <boderiotrigan@hotmail.com>
2021-01-26 15:20:26 +0100

    mise en place de l'app

---
+++ src/components/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = () => {
+    return ( 
+        <h2 class="notFound">Il n'y a rien sur cette URL</h2>
+     );
+}
+ 
+export default NotFound;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Connexion from './components/Connexion'
+import NotFound from './components/NotFound'
 import reportWebVitals from './reportWebVitals';
+
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Connexion} />
+      <Route path="/pseudo/:pseudo" component={App} />
+      <Route component={NotFound} />
+    </Switch>
+  </Router>
+)
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
